47c114c14dc9a97c41f94f3b1ca660afb78aa75f
irin <rhkrtlsd2486@naver.com>
2026-08-11 19:08:36 +0900

    test: use final 0.3.4 snapshot in control-plane fixtures

--- tests/test_decision_control_plane.py
+++ tests/test_decision_control_plane.py
@@ -7,7 +7,7 @@ from ptsip.app.store import DecisionStore
 from ptsip.clarification.resolution import DecisionAnswer, parse_answer, project_payload, validate_answer
 
 
-SPEC_REVISION = "ccee8cd5e26e92d31a2b93a86157c03d9b796b2c"
+SPEC_REVISION = "afba3531e23d96c21b7216e49614b839158ca7d5"
 
 
 class FakeGitHub:
